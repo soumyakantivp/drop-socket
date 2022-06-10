073caa83f8ae292ea5c45a5d3bf3efbc2d753887
soumyakantivp <soumyakantivp@gmail.com>
2022-06-10 10:20:32 +0530

    notification reciever added

--- src/main/java/com/connection/dropsocket/controller/InterceptController.java
+++ src/main/java/com/connection/dropsocket/controller/InterceptController.java
@@ -1,26 +1,29 @@
 package com.connection.dropsocket.controller;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InterceptController {
 	@GetMapping("/webhook")
-	public ResponseEntity<String> usingResponseEntityBuilderAndHttpHeaders(@RequestParam String challenge) {
-	    HttpHeaders responseHeaders = new HttpHeaders();
-	    responseHeaders.clear();
-	    responseHeaders.set("Content-Type", 
-	      "text/plain");
-	    responseHeaders.set("X-Content-Type-Options", 
-	  	      "nosniff");
+	public ResponseEntity<String> verify(@RequestParam String challenge) {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.clear();
+		responseHeaders.set("Content-Type", "text/plain");
+		responseHeaders.set("X-Content-Type-Options", "nosniff");
 
-	    return ResponseEntity.ok()
-	      .headers(responseHeaders)
-	      .body(challenge);
+		return ResponseEntity.ok().headers(responseHeaders).body(challenge);
+	}
+
+	@PostMapping(value = "/webhook", consumes = "text/plain")
+	public String updatePerson(@RequestBody String payload) {
+		
+		System.out.println(payload);
+		return "redirect:/test";
 	}
 }
