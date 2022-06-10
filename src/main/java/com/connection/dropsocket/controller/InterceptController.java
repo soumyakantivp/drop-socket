d5e1a66c0d8bef64403a0c1cb0032a64b0b4324c
soumyakantivp <soumyakantivp@gmail.com>
2022-06-10 11:12:31 +0530

    notification reciever changes

--- src/main/java/com/connection/dropsocket/controller/InterceptController.java
+++ src/main/java/com/connection/dropsocket/controller/InterceptController.java
@@ -2,15 +2,17 @@ package com.connection.dropsocket.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class InterceptController {
 	@GetMapping("/webhook")
+	@ResponseBody
 	public ResponseEntity<String> verify(@RequestParam String challenge) {
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.clear();
@@ -20,7 +22,7 @@ public class InterceptController {
 		return ResponseEntity.ok().headers(responseHeaders).body(challenge);
 	}
 
-	@PostMapping(value = "/webhook", consumes = "text/plain")
+	@PostMapping(value = "/webhook", consumes = "application/json")
 	public String updatePerson(@RequestBody String payload) {
 		
 		System.out.println(payload);
