d04e77e4656d886064069cac772458faa9c28df5
soumyakantivp <soumyakantivp@gmail.com>
2022-06-20 10:58:46 +0530

    cors added

--- src/main/java/com/connection/dropsocket/DropSocketApplication.java
+++ src/main/java/com/connection/dropsocket/DropSocketApplication.java
@@ -2,12 +2,17 @@ package com.connection.dropsocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DropSocketApplication {
+public class DropSocketApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DropSocketApplication.class, args);
 	}
-
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
 }
