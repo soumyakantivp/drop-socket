d07ca19818fb2f47b6e847e81712491116a66550
soumyakantivp <soumyakantivp@gmail.com>
2022-06-20 09:52:12 +0530

    removed dropbox dependency

--- src/main/java/com/connection/controller/InterceptController.java
+++ src/main/java/com/connection/controller/InterceptController.java
@@ -1,7 +1,6 @@
-package com.connection.dropsocket.controller;
+package com.connection.controller;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/connection/dropsocket/auth/test.app
+++
@@ -1,4 +0,0 @@
-{
-  "key": "98gl7lfdsg4a9iy",
-  "secret": "mpfoc369q56klxy"
-}
