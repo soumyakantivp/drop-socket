117b5c98c0c66d20f262e3e5235865c379ba46af
soumyakantivp <soumyakantivp@gmail.com>
2022-06-21 11:39:52 +0530

    C:/Program Files/Git/webhook post modified 3

--- src/main/java/com/connection/dropsocket/controller/InterceptController.java
+++ src/main/java/com/connection/dropsocket/controller/InterceptController.java
@@ -1,10 +1,12 @@
 package com.connection.dropsocket.controller;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,12 +35,22 @@ public class InterceptController {
 	@PostMapping(value = "/webhook", consumes = "application/json")
 	public ResponseEntity<String> updatePerson(@RequestBody String payload) {
 		//ResponseEntity r = new ResponseEntity(HttpStatus.OK);
-	
-		String url = "https://sokt.io/c/app/qy41HjusBSyHrbyLJ3xe/ep";
+		try {
+			String url = "https://sokt.io/c/app/qy41HjusBSyHrbyLJ3xe/ep";
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET,entity,String.class);
+
+            System.out.println(response);
+            return response;
+        } catch (Exception ex) {
+           ex.printStackTrace();
+        }
+		return null;
 		
-		String result = restTemplate.getForObject(url, String.class);
-		System.out.println(result);
-	
-		return new ResponseEntity<String> (HttpStatus.OK);
 	}
 }
